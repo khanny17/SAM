a2cec06962b2ac0ac74af385e8e2d53976e9e66b
Kevin <kph1958@rit.edu>
2016-09-24 16:29:30 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var inject = require('gulp-inject');
+var clean = require('gulp-clean');
+var series = require('stream-series');
+var sass = require('gulp-sass');
+var exec = require('child_process').exec;
+var nodemon = require('gulp-nodemon');
+
+
+// --- Paths ---
+
+var paths = {
+    
+
+    jshintPaths: ['public/js/**/*.js'],
+
+};
+
+var sources = {
+
+    appjs: 'public/js/app.js',
+    angularFiles:  ['public/js/**/*'],
+    index: 'public/index.html',
+    injectedJs:  ['public/js/**/*.js', '!public/js/app.js'],
+    libs: 'public/libs/**/*',
+    sass: 'public/sass/**/*.scss',
+    server: 'server.js',
+    views: 'public/views/**/*'
+
+};
+
+var dest = {
+
+    css: 'dist/public/css',
+    dist: 'dist/',
+    angularFiles: 'dist/public/js',
+    libs: 'dist/public/libs',
+    public: 'dist/public',
+    views: 'dist/public/views'
+
+};
+
+// -------------
+
+
+
+// --- Testing, Linting, Etc ---
+
+//Run jshint on our javascript files
+gulp.task('jshint', function(){
+    return gulp.src(paths.jshintPaths)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// -----------------------------
+
+
+
+// --- Move Tasks ---
+
+gulp.task('libs', function(){
+    return gulp.src(sources.libs)
+    .pipe(gulp.dest(dest.libs));
+});
+
+gulp.task('views', function(){
+    return gulp.src(sources.views)
+    .pipe(gulp.dest(dest.views));
+});
+
+gulp.task('server', function(){
+    return gulp.src(sources.server)
+    .pipe(gulp.dest(dest.dist));
+});
+
+gulp.task('angularFiles', function(){
+    return gulp.src(sources.angularFiles)
+    .pipe(gulp.dest(dest.angularFiles));
+});
+
+gulp.task('move', ['libs', 'views', 'server', 'angularFiles']);
+
+// ------------------
+
+
+
+// --- Compilation Tasks ---
+
+//Set up index.html, injecting required js files
+gulp.task('index', function(){
+    return gulp.src(sources.index)
+    .pipe(inject(series(gulp.src(sources.injectedJs, {read:false}), gulp.src(sources.appjs, {read:false}) ), {relative:true}))
+    .pipe(gulp.dest(dest.public))
+});
+
+gulp.task('sass', function(){
+    return gulp.src(sources.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(dest.css));
+});
+
+gulp.task('compile', ['index', 'sass']);
+
+// -------------------------
+
+
+gulp.task('build', ['compile', 'move']);
+
+
+gulp.task('clean', function(){
+    return gulp.src(dest.dist)
+    .pipe(clean());
+});
+
+gulp.task('default', ['jshint', 'build']);
+
+
+gulp.task('watch', function() {
+    gulp.watch(sources.sass, ['sass']);
+
+    gulp.watch(sources.server, ['server']); 
+
+    gulp.watch(sources.angularFiles, ['angularFiles']);
+});
+
+gulp.task('dev', ['watch'], function(){
+    nodemon({
+        script: 'dist/server.js'
+    });
+    
+});
---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// Modules 
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+
+// Configuration
+    
+// Server port
+var port = process.env.PORT || 8080; 
+
+// get all data/stuff of the body (POST) parameters
+// parse application/json 
+app.use(bodyParser.json()); 
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override')); 
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
+
+
+
+// --- Api Routes --- 
+
+var api = [
+    //{ route: 'example', file: require(__dirname + '/app/api/example') },
+];
+
+//Mounts routes for each api service in the above list
+api.forEach(function(service){
+    var router = express.Router();      //Create a new Router object
+    service.file.init(router);           //Initialize the endpoints
+    app.use('/api/' + service.route, router); //Mount sub-api
+});
+
+// ------------------
+
+
+// Start App 
+app.listen(port);               
+
+console.log('Server running on ' + port);
