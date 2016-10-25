486ce80651d7f76a8089314cac05814b5c24e625
shehan peruma <shehan.peruma@gmail.com>
2016-10-25 17:33:27 -0400

    added functionality related to paper submission

--- public/js/submit-paper-controller.js
+++ public/js/submit-paper-controller.js
@@ -6,7 +6,7 @@
     .controller('submitPaperController', ['$scope', '$http','AuthService','$state', function($scope, $http, AuthService,$state) {
         
         $scope.paper = {};
-        $scope.title = "SAM 2017 - User Details";
+        $scope.title = "SAM 2017 - Submit Paper";
         $scope.users = [];
         $scope.contactAuthor = AuthService.authenticatedUser().FirstName +" " + AuthService.authenticatedUser().LastName;
         $scope.paper.userID =  AuthService.authenticatedUser().ID;
